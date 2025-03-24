a50062559968455c77231649a3d163a1f5dce669
Sithura Myo <66086921+sithuramyo@users.noreply.github.com>
2025-03-24 11:13:25 +0630

    [Fixed] Auth Reponse

--- Interfaces/Features/Auth/AuthController.cs
+++ Interfaces/Features/Auth/AuthController.cs
@@ -6,7 +6,7 @@ namespace Interfaces.Features.Auth;
 public class AuthController(IAuthService service) : BaseController
 {
     [HttpPost]
-    [ProducesResponseType(typeof(ApiRequest<AuthRequestModel>),StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse<AuthResponseModel>),StatusCodes.Status200OK)]
     public async Task<ActionResult> Login(ApiRequest<AuthRequestModel> request)
     {
         var result = await service.LoginAsync(request.Request);

d8484e60176c00f007b1afe5ca6f0c8f6fc7db84
Sithura Myo <66086921+sithuramyo@users.noreply.github.com>
2025-03-31 10:21:02 +0630

    [Complete] Prerequisites Api in Common Api

--- Cores/Features/Commons/ICommonService.cs
+++ Cores/Features/Commons/ICommonService.cs
@@ -6,4 +6,5 @@ public interface ICommonService
 {
     Task<ApiResponse<DepartmentsResponseModel>> GetDepartmentsAsync();
     Task<ApiResponse<CoursesResponseModel>> GetCoursesAsync();
+    Task<ApiResponse<PrerequisiteResponseModel>> GetPrerequisitesAsync();
 }
---
+++ Shares/Models/Commons/PrerequisiteResponseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Shares.Models.Commons;
+
+public class PrerequisiteResponseModel
+{
+    public List<Prerequisites> Prerequisites { get; set; } = [];
+}
+
+public class Prerequisites
+{
+    public string Id { get; set; }
+    public string RequiredCourseCode { get; set; }
+    public string RequiredMinimumGrade { get; set; }
+    public bool IsMandatory { get; set; }
+    public string Notes { get; set; }
+}
